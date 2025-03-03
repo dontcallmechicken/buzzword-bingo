98ccac9923c57a0ecadfa1e1f22e49b0bbde420f
DontCallMeChicken <dcmc@bossmail.de>
2025-03-03 21:45:15 +0100

    add ters

--- cards/nemi/words.js
+++ cards/nemi/words.js
@@ -11,6 +11,7 @@ Gothic
 K&ouml;pfchen machen
 Shisha Kohle
 Leon
+Ich habe Hunger
 Pizza
 Wurst&shy;br&ouml;ttchen
 Es ist so dumm!
@@ -20,5 +21,9 @@ verliert bei Counter Strike
 spricht Englisch
 Dieser Ska!
 stirbt im Spiel
+Hat bock gemacht
+Ach naja, ist OK
+tats&auml;chlich
+so m&auml;&szlig;ig
 
 `;
